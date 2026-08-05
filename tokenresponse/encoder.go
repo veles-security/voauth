751f86b71929870aab02bf0031201135725b1964
Paweł Gąsiorowski <pgasiorowski@bluesoft.com>
2026-08-05 12:44:13 +0200

    token response encoder and writer

---
+++ tokenresponse/encoder.go
@@ -0,0 +1,109 @@
+package tokenresponse
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/veles-security/vapi"
+	"github.com/veles-security/voauth/jwt"
+	"github.com/veles-security/voauth/token"
+)
+
+type Encoder struct {
+	tokenEncoder token.AnyTokenEncoder
+}
+
+type EncoderConfigOption func(*Encoder) error
+
+type EncodeFunc func(ctx context.Context, artifact *TokenResponse, representation *TokenResponseRepresentation) error
+
+type EncoderOption func(next EncodeFunc) EncodeFunc
+
+func NewEncoder(configOptions ...EncoderConfigOption) (*Encoder, error) {
+	writer := &Encoder{}
+	for _, option := range configOptions {
+		if option == nil {
+			return nil, vapi.NewErrorCategory(vapi.ErrMisconfigured, errors.New("nil writer config option"))
+		}
+		if err := option(writer); err != nil {
+			return nil, vapi.NewErrorCategory(vapi.ErrMisconfigured, err)
+		}
+	}
+	if writer.tokenEncoder == nil {
+		tokenEcoder, err := jwt.NewEncoder()
+		if err != nil {
+			return nil, err
+		}
+		writer.tokenEncoder = tokenEcoder
+	}
+	return writer, nil
+}
+
+// WriteArtifact implements [vapi.Encoder].
+func (e *Encoder) Encode(ctx context.Context, artifact *TokenResponse, options ...EncoderOption) ([]byte, error) {
+	if e == nil || e.tokenEncoder == nil {
+		return nil, vapi.NewErrorCategory(vapi.ErrMisconfigured, errors.New("cannot encode token response with nil token encoder"))
+	}
+	if artifact == nil {
+		return nil, vapi.NewErrorCategory(vapi.ErrMalformed, errors.New("cannot encode nil token response"))
+	}
+	if artifact.AccessToken == nil {
+		return nil, vapi.NewErrorCategory(vapi.ErrMalformed, errors.New("cannot encode token response with nil access token"))
+	}
+
+	representation := &TokenResponseRepresentation{}
+	next := e.encode
+	for index := len(options) - 1; index >= 0; index-- {
+		if options[index] == nil {
+			return nil, vapi.NewErrorCategory(vapi.ErrMisconfigured, fmt.Errorf("nil encoder option at index %d", index))
+		}
+		next = options[index](next)
+		if next == nil {
+			return nil, vapi.NewErrorCategory(vapi.ErrMisconfigured, fmt.Errorf("encoder option at index %d returned nil EncodeFunc", index))
+		}
+	}
+	if err := next(ctx, artifact, representation); err != nil {
+		return nil, err
+	}
+	payload, err := json.Marshal(representation)
+	if err != nil {
+		return nil, vapi.NewErrorCategory(vapi.ErrMalformed, fmt.Errorf("encode token response representation: %w", err))
+	}
+	return payload, nil
+}
+
+func (e *Encoder) encode(ctx context.Context, artifact *TokenResponse, representation *TokenResponseRepresentation) error {
+	representation.TokenType = artifact.TokenType
+	representation.ExpiresIn = int64(artifact.ExpiresIn / time.Second)
+	representation.Scope = artifact.Scope
+	representation.IssuedTokenType = artifact.IssuedTokenType
+	representation.Resources = artifact.Resources
+
+	if artifact.AccessToken != nil {
+		accessToken, err := e.tokenEncoder.EncodeAnyToken(ctx, artifact.AccessToken)
+		if err != nil {
+			return vapi.NewErrorCategory(vapi.ErrMalformed, fmt.Errorf("encode access token: %w", err))
+		}
+		representation.AccessToken = string(accessToken)
+	}
+	if artifact.RefreshToken != nil {
+		refreshToken, encodeErr := e.tokenEncoder.EncodeAnyToken(ctx, artifact.RefreshToken)
+		if encodeErr != nil {
+			return vapi.NewErrorCategory(vapi.ErrMalformed, fmt.Errorf("encode refresh token: %w", encodeErr))
+		}
+		representation.RefreshToken = string(refreshToken)
+	}
+	if artifact.IdToken != nil {
+		idToken, encodeErr := e.tokenEncoder.EncodeAnyToken(ctx, artifact.IdToken)
+		if encodeErr != nil {
+			return vapi.NewErrorCategory(vapi.ErrMalformed, fmt.Errorf("encode ID token: %w", encodeErr))
+		}
+		representation.IdToken = string(idToken)
+	}
+	return nil
+}
+
+var _ vapi.Encoder[*TokenResponse, EncoderOption] = &Encoder{}
--- tokenresponse/writer.go
+++ tokenresponse/writer.go
@@ -3,15 +3,14 @@ package tokenresponse
 import (
 	"context"
 	"errors"
+	"fmt"
 	"net/http"
 
 	"github.com/veles-security/vapi"
-	"github.com/veles-security/voauth/jwt"
-	"github.com/veles-security/voauth/token"
 )
 
 type Writer struct {
-	tokenEncoder token.AnyTokenEncoder
+	encoder vapi.Encoder[*TokenResponse, EncoderOption]
 }
 
 type WriterConfigOption func(*Writer) error
@@ -30,19 +29,50 @@ func NewWriter(configOptions ...WriterConfigOption) (*Writer, error) {
 			return nil, vapi.NewErrorCategory(vapi.ErrMisconfigured, err)
 		}
 	}
-	if writer.tokenEncoder == nil {
-		tokenEcoder, err := jwt.NewEncoder()
+	if writer.encoder == nil {
+		tokenEcoder, err := NewEncoder()
 		if err != nil {
 			return nil, err
 		}
-		writer.tokenEncoder = tokenEcoder
+		writer.encoder = tokenEcoder
 	}
 	return writer, nil
 }
 
 // WriteArtifact implements [vapi.Writer].
 func (w *Writer) WriteArtifact(ctx context.Context, carrierWriter http.ResponseWriter, artifact *TokenResponse, options ...WriterOption) error {
-	panic("unimplemented")
+	if w == nil || w.encoder == nil {
+		return vapi.NewErrorCategory(vapi.ErrMisconfigured, errors.New("cannot write token response with nil encoder"))
+	}
+
+	next := func(ctx context.Context, carrierWriter http.ResponseWriter, artifact *TokenResponse) error {
+		payload, err := w.encoder.Encode(ctx, artifact)
+		if err != nil {
+			return err
+		}
+
+		header := carrierWriter.Header()
+		header.Set("Content-Type", "application/json;charset=UTF-8")
+		header.Set("Cache-Control", "no-store")
+		header.Set("Pragma", "no-cache")
+		carrierWriter.WriteHeader(http.StatusOK)
+		if _, err = carrierWriter.Write(payload); err != nil {
+			return vapi.NewErrorCategory(vapi.ErrMalformed, fmt.Errorf("write token response: %w", err))
+		}
+		return nil
+	}
+
+	for index := len(options) - 1; index >= 0; index-- {
+		if options[index] == nil {
+			return vapi.NewErrorCategory(vapi.ErrMisconfigured, fmt.Errorf("nil writer option at index %d", index))
+		}
+		next = options[index](next)
+		if next == nil {
+			return vapi.NewErrorCategory(vapi.ErrMisconfigured, fmt.Errorf("writer option at index %d returned nil WriteFunc", index))
+		}
+	}
+
+	return next(ctx, carrierWriter, artifact)
 }
 
 var _ vapi.Writer[http.ResponseWriter, *TokenResponse, WriterOption] = &Writer{}
