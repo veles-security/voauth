50219695339498833bfc3c87386f448489322da7
Paweł Gąsiorowski <pgasiorowski@bluesoft.com>
2026-08-05 15:18:24 +0200

    removed deprecated option

--- jwks/jwks.go
+++ jwks/jwks.go
@@ -3,7 +3,6 @@ package jwks
 import (
 	"github.com/veles-security/vapi/sig"
 	"github.com/veles-security/voauth/jwk"
-	"github.com/veles-security/voauth/jwt"
 )
 
 const JwksKind = "oauth2:jwks"
@@ -17,12 +16,6 @@ type JwksOption func(*Jwks)
 func WithKeyFromSigner(signer *sig.Signer) JwksOption {
 	return func(j *Jwks) {
 		j.Keys = append(j.Keys, *jwk.NewJwk(signer.Alg, signer.Key.Public(), signer.Kid))
-	}
-}
-
-func WithKeyFromJwtSigner(signer *jwt.Signer) JwksOption {
-	return func(j *Jwks) {
-		j.Keys = append(j.Keys, *jwk.NewJwk(signer.Alg(), signer.Public(), signer.Kid()))
 	}
 }
 
